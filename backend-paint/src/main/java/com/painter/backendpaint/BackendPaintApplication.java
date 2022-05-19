fac07bf73b7bf8c779a01a643a20dd073f6a7f38
MohamedMetwalli5 <mohamedmetwalli5@gmail.com>
2022-05-19 13:30:16 +0200

    updates!

---
+++ backend-paint/src/main/java/com/painter/backendpaint/BackendPaintApplication.java
@@ -0,0 +1,13 @@
+package com.painter.backendpaint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPaintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPaintApplication.class, args);
+	}
+
+}
